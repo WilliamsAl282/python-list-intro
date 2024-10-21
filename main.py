b4a13c35ef6480a99944ce3f6b8b8721f005c255
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-10-21 10:49:01 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# 10/21/2024
+# Alexander Williams
+# Web and App Development
+
+
+
+# 3-1 Names
+names = ['Doug', 'JJ', 'Arthur', 'Kaytrin']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+# 3-2 Greetings
+
+print(f'I have a friend named {names[0]} ')
+print(f'I have a friend named {names[1]} ')
+print(f'I have a friend named {names[2]} ')
+print(f'I have a friend named {names[3]} ')
+
+# 3-3 Your Own List
+
+Transportation = ['car','bus','boat']
+
+
+print(f'I would like to be taken to school by a {Transportation[0]}')
+print(f'I would like to be taken to school by a {Transportation[1]}')
+print(f'I would like to go fishing in a {Transportation[2]}')
+print(f'I would like to own a {Transportation[0]} made by Tesla')
+print(f'I like being in a travel {Transportation[1]} because they are cool and comfrotable to be in')
